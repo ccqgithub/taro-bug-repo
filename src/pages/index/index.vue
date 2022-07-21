8d9309388e4ebfa90be55c94d25f247190364728
chen season <chenseason@chendeMacBook-Pro.local>
2022-07-21 17:12:51 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <View class="index">
+    <Counter />
+  </View>
+</template>
+
+<script setup lang="ts">
+import { View } from '@tarojs/components'
+import './index.scss'
+import Counter from '../../components/Counter.vue'
+</script>
